0403e815882d75bd81f26c40da9c965cd5a67ea5
LUISROMERO-123 <144745167+LUISROMERO-123@users.noreply.github.com>
2023-11-30 17:25:59 -0600

    Add files via upload

---
+++ EVA3_5_REFERENCIA/src/eva3_5_referencia/EVA3_5_REFERENCIA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_5_referencia;
+
+/**
+ *
+ * @author g61
+ */
+public class EVA3_5_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          // TODO code application logic here
+        
+        int [] arreglo = new int [10];
+        System.out.println(arreglo);
+    for(int i : arreglo){
+        System.out.println(i);
+    }
+        int i = 10;
+        System.out.println(i);
+    
+        
+    }
+    
+}
